dad9ee96715e2c0d3bd1a676411e73e5754e414f
jdeluna06 <jdeluna0672@gmail.com>
2024-11-14 18:35:21 -0800

    set up cypress and component-quiz

---
+++ cypress/e2e/quiz.cy.js
@@ -0,0 +1,37 @@
+describe("User Takes Quiz", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should start the quiz and display the first question", () => {
+    cy.mount(<Quiz />);
+    cy.get("button").contains("Start Quiz").click();
+    cy.get(".card").should("be.visible");
+    cy.get("h2").should("not.be.empty");
+  });
+  it("should answer questions and complete the quiz", () => {
+    cy.mount(<Quiz />);
+    cy.get("button").contains("Start Quiz").click();
+
+    // Answer questions
+    cy.get("button").contains("1").click();
+
+    // Verify the quiz completion
+    cy.get(".alert-success").should("be.visible").and("contain", "Your score");
+  });
+
+  it("should restart the quiz after completion", () => {
+    cy.mount(<Quiz />);
+    cy.get("button").contains("Start Quiz").click();
+
+    // Answer questions
+    cy.get("button").contains("1").click();
+
+    // Restart the quiz
+    cy.get("button").contains("Take New Quiz").click();
+
+    // Verify the quiz is restarted
+    cy.get(".card").should("be.visible");
+    cy.get("h2").should("not.be.empty");
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -9,16 +9,5 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/_tests_/setup.ts'
   },
-  server: {
-    port: 3001,
-    host: '127.0.0.1',
-    open: false,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
-  }
+  
 })
